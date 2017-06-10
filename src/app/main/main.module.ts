04cbeb95e034bf5df5ca5a93fc4318a64d1fa06b
Bằng Ngô <ngotribang@gmail.com>
2017-06-10 10:49:40 +0700

    Config routing system for angular 2

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { ProductCategoryComponent } from './product-category/product-category.component';
 import { FunctionComponent } from './function/function.component';
 import { RoleComponent } from './role/role.component';
 import { ProductComponent } from './product/product.component';
+import { mainRoutes } from './main.routes';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(mainRoutes)
   ],
   declarations: [MainComponent, HomeComponent, ProductCategoryComponent, FunctionComponent, RoleComponent, ProductComponent]
 })
