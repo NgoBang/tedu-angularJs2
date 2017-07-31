c1c2f1b309e6888f6e33f5ac0ed366750bd5af4e
Bằng Ngô <ngotribang@gmail.com>
2017-07-31 22:51:33 +0700

    Lesson 32: Get dynamic menu from db

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,18 +5,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainRoutes } from './main.routes';
 import { HomeModule } from './home/home.module';
 import { MainComponent } from './main.component';
+import { TopMenuComponent } from '../shared/top-menu/top-menu.component';
+import { SidebarMenuComponent } from '../shared/sidebar-menu/sidebar-menu.component';
 import { UserModule } from './user/user.module';
 import { AuthenService } from '../core/services/authen.service';
 import { UtilityService } from '../core/services/utility.service';
 
 @NgModule({
+  declarations: [
+    MainComponent,
+    SidebarMenuComponent,
+    TopMenuComponent
+  ],
   imports: [
     CommonModule,
     UserModule,
     HomeModule,
     RouterModule.forChild(MainRoutes)
   ],
-  providers: [AuthenService, UtilityService],
-  declarations: [MainComponent]
+  providers: [AuthenService, UtilityService]
 })
 export class MainModule { }
