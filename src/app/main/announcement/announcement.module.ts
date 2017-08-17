4e89146ec72caad6ed6d1de0f82ecfabf74637a8
Bằng Ngô <ngotribang@gmail.com>
2017-08-17 20:19:42 +0700

    Lesson 40: Integrate signalR to application

---
+++ src/app/main/announcement/announcement.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnnouncementComponent } from './announcement.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AnnouncementComponent]
+})
+export class AnnouncementModule { }
